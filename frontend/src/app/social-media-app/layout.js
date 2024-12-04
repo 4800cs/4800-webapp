022ad19a8822227df1af0dedad6e2f421156a7c5
PhiNguyen357 <phinguyen7527@gmail.com>
2024-12-04 12:54:14 -0800

    commented out shooting star

--- frontend/src/app/social-media-app/layout.js
+++ frontend/src/app/social-media-app/layout.js
@@ -7,7 +7,7 @@ import styles from '../styles/app.layout.css';
 import Link from 'next/link';
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
+  /*useEffect(() => {
     const container = document.createElement('div');
     container.className = 'shooting-stars-container';
     document.body.appendChild(container);
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
     return () => {
       container.remove();
     };
-  }, []);
+  }, []);*/
   return (
     <>
       <Navbar/>
